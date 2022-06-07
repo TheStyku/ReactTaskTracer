41c5bd6b04067db8491ad5975d60e4dc2d911b12
Patryk Styczyski <patrykszu1994@o2.pl>
2022-06-07 20:03:19 +0200

    master

--- task_tracer/src/components/Historia.js
+++ task_tracer/src/components/Historia.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 function Historia({historia}) {
   return (
-      
-      historia.name==='Zaupy'?(<div>Brak</div>):
-       <div className='Tak'> {historia.map((hist, id)=>( 
-         <ul style={{listStyleType : 'none', maxWidth:'100%', padding:'0'}}>
-         <li key={id}>
-           <span className='Historia1'>{hist.name}</span><span className='Historia2'> {hist.value}</span>
+    <ul style={{listStyleType : 'none', maxWidth:'100%', padding:'0'}}>     
+      {Array.isArray(historia) && !historia.length===true?(<div style={{textAlign: 'center'}}>Brak</div>):
+       < > {historia.map((hist, id)=>( 
+         <li className='Cards' key={id}>
+           <><span>{hist.name}</span></>< > <span style={{float: 'right'}}>{hist.value}</span> </>
          </li>
-         </ul>))}
-       </div>
+        ))}
+       </>}
+       </ul>
   )
 }
 
--- task_tracer/src/components/Transakcja.js
+++ task_tracer/src/components/Transakcja.js
@@ -1,26 +1,19 @@
-import React,{ useState }  from 'react'
+import React, { useState } from 'react'
 
-function Transakcja({setHistoria,setSaldo}) {
-  const [inputs, setInputs] = useState({
-    name: '', 
-    value: 0 
-  });
-
-  const handleChange = (e) => {
-    setInputs(values => ({...values,  [e.target.name]: e.target.value}))
-  }
+function Transakcja({ setHistoria, setSaldo }) {
+  const [name, SetName] = useState("");
+  const [number, setNumber] = useState(0);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setHistoria((historia)=>[...historia,{name: inputs.name, value: inputs.value}]);
-    if(inputs.value<0){
-      //setSaldo((saldo) => [...saldo,{wyplywy: inputs.value}])
+    setHistoria((historia) => [...historia, { name: name, value: number }]);
+    if (number < 0) {
       setSaldo(previousState => {
-        return { ...previousState, wyplywy: inputs.value  }
+        return { ...previousState, wyplywy: number + previousState.wyplywy }
       })
-    }else{
+    } else {
       setSaldo(previousState => {
-        return { ...previousState, wplywy: inputs.value }
+        return { ...previousState, wplywy: number + previousState.wplywy }
       })
     }
   }
@@ -28,24 +21,25 @@ function Transakcja({setHistoria,setSaldo}) {
     <>
       <form onSubmit={handleSubmit}>
         <label>
-        <h3>Rodzaj transakcji</h3>
-          <input 
-          className='input' 
-          type='text'
-          name = 'name' 
-          placeholder='Enter text' 
-          value={inputs.name || ""} 
-          onChange={handleChange}
+          <h3>Rodzaj transakcji</h3>
+          <input
+            className='input'
+            type='text'
+            name='name'
+            placeholder='Enter text'
+            value={name || ""}
+            onChange={(e) => SetName(e.target.value)}
           />
         </label>
         <label>
-        <h3>Użyte środki<br />Z znakiem - jeśli pobierasz kwote</h3>
-          <input 
-          className='input' 
-          type='number'
-          placeholder='Podaj kwote'
-          name='value'
-          onChange={handleChange}
+          <h3>Użyte środki<br />Z znakiem - jeśli pobierasz kwote</h3>
+          <input
+            className='input'
+            type='number'
+            placeholder='Podaj kwote'
+            name='value'
+            value={number || ''}
+            onChange={(e) => setNumber(e.target.valueAsNumber)}
           />
         </label>
         <br />
